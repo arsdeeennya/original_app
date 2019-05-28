49f3c939d8008d18025f663e4e2d68a4e15a684f
EC2 Default User <ec2-user@ip-172-31-15-6.ap-southeast-1.compute.internal>
2019-05-28 03:17:41 +0000

    Impoove: user_specのリファクタリング

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,44 +2,41 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   
+  before do
+    @user = User.create(
+      name: "Example User", 
+      email: "user@example.com",
+      password: "foobar",
+      password_confirmation: "foobar")
+  end
+  
+  
   # 名、メール、パスワード、パスワード（確認）があれば有効である
   it 'is valid with a name, email, password, password_confirmation' do
-    user = User.new(
-      name:'hogehoge',
-      email: 'hogehoge@yahoo.co.jp',
-      password: 'foobar',
-      password_confirmation: 'foobar')
-      
-    expect(user).to be_valid
+    expect(@user).to be_valid
   end  
   
   #名前がなくても有効である
   it 'is valid without a name' do
-    user = User.new(name: nil)
-    user.valid?
-    expect(user.errors[:name]).to_not include("can't be blank")
+    @user = User.new(name: nil)
+    @user.valid?
+    expect(@user.errors[:name]).to_not include("can't be blank")
   end
   
   # メールアドレスがなければ無効である
   it 'is invalid without a email' do
-    user = User.new(email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
+    @user = User.new(email: nil)
+    @user.valid?
+    expect(@user.errors[:email]).to include("can't be blank")
   end
   
   # メールアドレスが重複したら無効である
   it 'is invalid with a duplicate email address' do
-    User.create(
-      name: 'hogehoge',
-      email: 'hogehoge@yahoo.co.jp',
+    user = User.new(
+      name: 'Example User',
+      email: 'user@example.com',
       password: 'foobar',
       password_confirmation: 'foobar')
-      
-    user = User.new(
-      name: 'hogetaro',
-      email: 'hogehoge@yahoo.co.jp',
-      password: 'hogetaro',
-      password_confirmation: 'hogetaro')
       
     user.valid?
     expect(user.errors[:email]).to include("has already been taken")

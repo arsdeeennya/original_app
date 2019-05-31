14d6dc1c5c1145f9b67f14a9183c0e54549a1737
EC2 Default User <ec2-user@ip-172-31-15-6.ap-southeast-1.compute.internal>
2019-05-31 14:11:00 +0000

    Improve:routes.rbをコントローラごとに整理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,14 @@
 class UsersController < ApplicationController
   # ブラウザ以外にもpatchリクエストを送られた場合もある。だからupdateアクションも含まれている。
   before_action :logged_in_user, only: %i[index edit update destroy]
-  before_action :correct_user, only: %i[edit update]
+  before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy]
-
+  
+  
   def index
     @users = User.paginate(page: params[:page])
   end
-
+  
   def show
     @user = User.find(params[:id])
   end
@@ -44,13 +45,13 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-
+  
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = 'User deleted'
+   User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
     redirect_to users_url
   end
-
+  
   private
 
   def user_params
@@ -65,15 +66,15 @@ class UsersController < ApplicationController
       redirect_to login_url
     end
   end
-
+  
   # 正しいユーザーかどうか確認
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
-
+  
   # 管理者かどうか確認
-  def admin_user
-    redirect_to(root_url) unless current_user.admin?
-  end
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,25 @@
 
 Rails.application.routes.draw do
   # get 'sessions/new', to: 'sessions#new'
-
+  
   root 'static_pages#home'
-  get '/home', to: 'static_pages#home'
-  get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-  resources :users
+  
+  controller :static_pages do
+    get '/home', to: 'static_pages#home'
+    get '/help', to: 'static_pages#help'
+    get '/about', to: 'static_pages#about'
+    get '/contact', to: 'static_pages#contact'
+  end
+  
+  
+  controller :sessions do
+    get    '/login',   to: 'sessions#new'
+    post   '/login',   to: 'sessions#create'
+    delete '/logout',  to: 'sessions#destroy'
+  end
+  
+  controller :users do
+    resources :users
+  end
+  
 end

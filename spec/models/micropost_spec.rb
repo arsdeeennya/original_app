dd0b86f27f4c7e44fbe07ec7154d22512d6fe516
EC2 Default User <[kkamazonkk@yahoo.co.jp]>
2019-06-09 19:26:08 +0000

    Add: user_specにexampleを追加

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe Micropost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe Micropost, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,35 +12,70 @@ RSpec.describe User, type: :model do
     )
   end
 
-  # 名、メール、パスワード、パスワード（確認）があれば有効である
-  it 'is valid with a name, email, password, password_confirmation' do
+  it '名前、メール、パスワード、パスワード（確認）があれば有効である' do
     expect(@user).to be_valid
   end
 
-  # 名前が空だと”ななしのごんべい”になる
-  it 'is valid without a name' do
+  it '名前が空だと、ななしのごんべい、になる' do
     @user = User.new(name: nil)
     @user.valid?
     expect(@user.name).to include("ななしのごんべい")
   end
 
-  # メールアドレスがなければ無効である
-  it 'is invalid without a email' do
+  it 'メールアドレスがなければ入力必須である' do
     @user = User.new(email: nil)
     @user.valid?
     expect(@user.errors[:email]).to include('は不正な値です')
   end
-
-  # メールアドレスが重複したら無効である
-  it 'is invalid with a duplicate email address' do
-    user = User.new(
-      name: 'Example User',
-      email: 'user@example.com',
-      password: 'foobar',
-      password_confirmation: 'foobar'
-    )
-
-    user.valid?
-    expect(user.errors[:email]).to include('はすでに存在します')
+  
+  it '名前は長すぎると無効である' do
+    @user.name = 'a' * 51
+    @user.valid?
+    expect(@user.errors[:name]).to include('は50文字以内で入力してください') 
   end
+  
+  it 'メールアドレスは長すぎては無効である' do
+    @user.email = 'a' * 244 + '@example.com'
+    @user.valid?
+    expect(@user.errors[:email]).to include('は255文字以内で入力してください')
+  end
+  
+  it '有効なメールアドレスは有効である' do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      @user.valid?
+      expect(@user.errors[:email]).to_not include('は不正な値です')
+    end
+  end
+  
+  it '無効なメールアドレスを無効である' do
+    valid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                        foo@bar_baz.com foo@bar+baz.com]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      @user.valid?
+      expect(@user.errors[:email]).to include('は不正な値です')
+    end
+  end
+  
+  it 'メールアドレスは一意である' do
+    duplicate_user = @user.dup
+    duplicate_user.valid?
+    expect(duplicate_user.errors[:email]).to include('はすでに存在します')
+  end
+
+  it 'パスワードは入力必須である' do
+    @user.password = @user.password_confirmation = ' ' * 6
+    @user.valid?
+    expect(@user.errors[:password]).to include('を入力してください')
+  end
+
+  it 'パスワードは短すぎてはいけない' do
+    @user.password = @user.password_confirmation = 'a' * 5
+    @user.valid?
+    expect(@user.errors[:password]).to include('は6文字以上で入力してください')
+  end
+  
 end

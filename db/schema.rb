1e8fbc3cf425d7b1655eebf31fc682e26eae38c1
EC2 Default User <[kkamazonkk@yahoo.co.jp]>
2019-06-11 08:57:32 +0000

    Add:Taskモデルo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190611013019) do
+ActiveRecord::Schema.define(version: 20190611085303) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20190611013019) do
   end
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

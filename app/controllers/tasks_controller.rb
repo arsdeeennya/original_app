c0e7cd82199e2c365a9e9fdcf4bff822f5f2c46a
EC2 Default User <[kkamazonkk@yahoo.co.jp]>
2019-06-12 09:20:15 +0000

    Add: パラメータを直接入力して検索することを防ぐ

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,6 @@ class TasksController < ApplicationController
   before_action :logged_in_user
   
   def index
-    # @tasks = current_user.tasks
     @q = current_user.tasks.ransack(params[:q])
     @tasks = @q.result(distinct: true)
   end

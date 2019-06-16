9af2833860fb51e26446fab7e8dd16315100f691
EC2 Default User <[kkamazonkk@yahoo.co.jp]>
2019-06-16 09:27:02 +0000

    Add: コントローラスペックにexpectを追加

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -7,21 +7,25 @@ RSpec.describe StaticPagesController, type: :controller do
     it "ステータスコードが200である" do
       get :home
       expect(response).to be_success
+      expect(response).to have_http_status '200'
     end
     
     it "ステータスコードが200である" do
       get :help
       expect(response).to be_success
+      expect(response).to have_http_status '200'
     end
     
     it "ステータスコードが200である" do
       get :about
       expect(response).to be_success
+      expect(response).to have_http_status '200'
     end
     
     it "ステータスコードが200である" do
       get :contact
       expect(response).to be_success
+      expect(response).to have_http_status '200'
     end
     
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe UsersController, type: :controller do
     it "ステータスコードが200である" do
       get :new
       expect(response).to be_success
+      expect(response).to have_http_status '200'
     end
     
   end
